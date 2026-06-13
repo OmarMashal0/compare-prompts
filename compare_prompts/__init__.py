88261a14e0d7b5c939d565662c9628781cf42397
Omar Mashal <omarzyad113@gmail.com>
2026-06-14 01:28:09 +0300

    feat: add __main__.py to support python -m execution

--- compare_prompts/__init__.py
+++ compare_prompts/__init__.py
@@ -21,7 +21,7 @@ from compare_prompts.display import print_table
 
 load_dotenv()
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 
 def compare(
---
+++ compare_prompts/__main__.py
@@ -0,0 +1,5 @@
+import sys
+from compare_prompts.cli import main
+
+if __name__ == "__main__":
+    sys.exit(main())
